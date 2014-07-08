71b7edcbbd89e3ba07ecd0231110d27bc9acc7bf
Peter Schneider-Manzell <peter@schneider-manzell.de>
2014-07-08 09:21:47 +0200

    Added simple AccessTokenTest

---
+++ src/test/groovy/org/simple/auth/model/AccessTokenTest.groovy
@@ -0,0 +1,29 @@
+package org.simple.auth.model
+
+import spock.lang.Specification
+
+/**
+ * @author Peter Schneider-Manzell
+ */
+class AccessTokenTest extends Specification {
+
+    AccessToken token
+
+    def "AccessToken requires network"() {
+        when:
+        token = new AccessToken(null, "testtoken")
+
+        then:
+        final NullPointerException exception = thrown()
+        exception != null
+    }
+
+    def "AccessToken requires token"() {
+        when:
+        token = new AccessToken("network", null)
+
+        then:
+        final NullPointerException exception = thrown()
+        exception != null
+    }
+}

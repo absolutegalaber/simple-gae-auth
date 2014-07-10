aaa3094e7b7d18cf977ac6fcfaceb2ad6ceb9f81
Josip Mihelko <josip.mihelko@googlemail.com>
2014-07-10 14:47:24 +0200

    Token Shadow Sub-Project

--- token-shadow/api/src/main/java/org/simple/auth/shadow/Account.java
+++
@@ -1,7 +0,0 @@
-package org.simple.auth.shadow;
-
-/**
- * Created by Josip.Mihelko @ Gmail
- */
-public class Account {
-}
---
+++ token-shadow/api/src/main/java/org/simple/auth/shadow/IAccount.java
@@ -0,0 +1,10 @@
+package org.simple.auth.shadow;
+
+import java.io.Serializable;
+
+/**
+ * Created by Josip.Mihelko @ Gmail
+ */
+public interface IAccount {
+    public Serializable getId();
+}
